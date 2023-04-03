f145c6cd7f88a97796a47bab7ed6004d63694b9b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2023-04-03 06:24:57 +0300

    show placeholder images while other assets are being downloaded

--- tv-program/tv-program.js
+++ tv-program/tv-program.js
@@ -1,21 +1,34 @@
 export let devices = {
     'crt tv with antenna': {
-        frame_url: url('frame1.png'),
+        frame: {
+            url: url('frame1.png'),
+            // base64 -w0 < tv-program/frame1-placeholder.png
+            placeholder: 'iVBORw0KGgoAAAANSUhEUgAAAGQAAABWBAMAAADLfi7BAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAPUExURUdwTB0eHI2TmmhsbDo6O4lvH+UAAAABdFJOUwBA5thmAAABzklEQVRYw83XUZKDIAwAUGbtBVg5gCs9gBAOUIz3P9MiriJoJdnZ7jTT/jg8iRDHIMSTaEiXsrg9DpceFSK68sJHVyP2QGriOEJXySGzrk76YtZBcDOrP0rYho6bVzGoGSgEOFt/MgpoZJ8LKa9sWEMkDfdRsls7ItllNlAJcvPajSTnJW4DN6/t3c3f6p4dQk/MsEJrYIUNxJDu3a5pQSCOTmz465kgIZ2ZhJtbOrGBaN86aUtySGdPTOvQqY3oMFqehopuJhImRGm3WeRFqEi0BEBnPlfiroiM5K7AeGfalRgS8XSCMTHlzAQpMQqRDhD9uBJfJ2GRAUH2HDIvmR9ZJGy/xJ5I2oXkZUkieVlSiPa4L0sKCStm74pF5hqzqSxlnYQ1DiPT7hNILEtINUYj3sMXkailLG1WlhQSHz+VJYHMiww6lSWFFGVJIUVZksgS/0Hsi4mG8J3QYHmzwPzjkLE3Vrc9oyx5s7Rni+xfQVT6or2QtO9I8I+IeVdC+O6XhNBdHIibnoKfHuZAILIQabljq4Rbp3RKrvuxnNQbUl0QUm+p+GQ/y53bJ49CI484Kz64LX88bXI6/vWUcv26xH3FRayHkht9kl+Sb8cM0RruZs2mAAAAAElFTkSuQmCC'
+        },
         snow: { width: 65, x: 13, y: 57 },
         program: { width: 63, x: 14, y: 45 }
     },
     'black crt tv': {
-        frame_url: url('frame2.png'),
+        frame: {
+            url: url('frame2.png'),
+            placeholder: 'iVBORw0KGgoAAAANSUhEUgAAAGQAAABhBAMAAADSARnUAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAPUExURUdwTH1zay0jHUw+NGJXTP3BxL8AAAABdFJOUwBA5thmAAAB+klEQVRYw+XYC47iMAwA0OzOCUJ9ARwOsGPnAFOc+59p8+sHmpa4EmilNUIIwasTE9o6xpigCZPiy3GO/IJMvBPOpa+4RAbHisCfSEhF6I8xv5Z3WOLwEHQz5n61T7GerW+lwW+7DdhUiv2SZkuaxRUfREkCH2XZDEu4ZtghMAEIsEKPZMilnStN+FChNIvCJH7IUkhcJizfI1goGUFcGjs54jiz+IsvEaASd8GuuOI4TFk6CZItBPrJxQ6oJGQlEkoEu7NUIirizhJWENIS+CSxJ0jQENQTun2QkJJc309cOsGeIPB+QioSuEz/BLFaclOT+0nSfeqTU8QpB3aChEqgf2AL6T5d6snlYwT15KYloidTFvwECaieyz9L5P8m/FYC6eYqk4uOXBVZaMnSG0MlinvLFekN1meZCXUTNxF8J8EwD8zbroCJHH4JatcEufGJBchrLPUzo93rvRqNZSGHHZuCeBYtabavr8nGdJDnKb0m0hzY+Nzr8urgPhtJj9xfrrLIwyHna9TS+0/d6hMpr+LLH+gen35VAGqQEcKYV0ZpWH0lQVj2p19ns1w7/aZmyxrLR3N4Z4fHda7XlzCOOYOfp7QptX8kzc2Kxh7ERH7jq50XdLi6MXBp7ybV3nWfl/KmktcsGGqSWAIvu8PMRLWnxD/mRPwF0Pv8QvZrco4AAAAASUVORK5CYII='
+        },
         snow: { width: 85, x: 10, y: 18 },
         program: { width: 83, x: 10, y: 14 }
     },
     'white crt monitor': {
-        frame_url: url('frame3.png'),
+        frame: {
+            url: url('frame3.png'),
+            placeholder: 'iVBORw0KGgoAAAANSUhEUgAAAGQAAABUBAMAAACGto/KAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAbUExURUdwTOPo6urv8i4jH9bb3t7j5Y+Ulqywsz03Md/qxlQAAAABdFJOUwBA5thmAAAA1UlEQVRYw+3YQQ6CMBAF0MGEvTNJ904TTuAVULeU2Bt4BBOv4LFtK4UCmrQbFzI/YdF0HklnNhQAH+uj3IMf43ZMKAnFUOnC7GFXSg7/RthmhzzpHWkfmWkdaQI5HzNzE9IA/YDwtjvGQoQIESJEyLaIfFxJx4rJ5ZmZ+0ja7ESiMTs6kqI0cuPzoZxiHkiVrELQXqfRnSxOOzSQepzRov3rgb0rO4hXeNL4nRAhs7u2p6l0r5HV7JXJwhij1ILUq6MyrVrSwdxYg+lvCIVokqVFZaN4AchoAbYBlm1SAAAAAElFTkSuQmCC'
+        },
         snow: { width: 85, x: 10, y: 10 },
         program: { width: 83, x: 10, y: 5 }
     },
     'imac g3': {
-        frame_url: url('frame4.png'),
+        frame: {
+            url: url('frame4.png'),
+            placeholder: 'iVBORw0KGgoAAAANSUhEUgAAAGQAAABnBAMAAAAEWPrJAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAbUExURUdwTNTX1qappYyOiQwOCmtqYoNOLEsyIbB3TD59duUAAAABdFJOUwBA5thmAAAB7ElEQVRYw+2Yu27CMBSGaStVrPYbYIuqY2Xj9gGgYo2anr1Tylipl6ydojx27TgE3+LESI0y8AsJSPzlXOIg/rNY+LrhnRZRcU4Z6cRd2YuvSVjU46AlrsIAC0Z7aBCEgyFYOMUmCAoyp8JsJNMIIjH5CBpCKHM7MBmC47WMQXhELSIvFF93lNwPkOkei6/dgPb7b4CcnJDNbpSeJ0bwXJGzyn8di1AuNELTETIJMtvEEhGUXgs6L0oSAvNG6FwRckFSkc0FmRHC5xplCmR7QugkCPlfBLL0KDNGUv72bPFZCEyJ4GmQp6QNk4J8GggHV6WSe9CwCWiEpezMSIvg8cwR6bVWlNFeBPlRmjcSQZzcuBAs6LIsxILWAthQFD+zwcTGmF03il1N730RJhLtNnENLyKrcONUNfatNT2yiVBtOxlRXrc9InIBuW14VRyMnClBw+m8QL7ARFYqTCgn3n4WwJmwELk86sc1kjsIoS9r1tsw8BLTua/7Tb8QzUN2HHfERwRtwzQhxiKq6U2L1X5pMhsYRBg7U0B0pOQ/zjK54HwruBdVIZkz6rotiqL0f4lKQx8BROq97JeHPGqmsFb9dN/lGRdZ3hVDenPndvdVdP2hrjyE1QOqQgPCpTxxqIu6Kg7yqvVvzxzxD363QErGq3+0AAAAAElFTkSuQmCC'
+        },
         snow: { width: 85, x: 10, y: 17 },
         program: { width: 83, x: 10, y: 10 }
     }
@@ -64,12 +77,20 @@ customElements.define('tv-program', class extends HTMLElement {
 
 #frame {
   width: 100%;
+  visibility: hidden;
   z-index: 1;
+}
+
+#frame-placeholder {
+  width: 100%;
+  image-rendering: pixelated;
 }
 </style>
 
-<img alt="device frame" id="frame" src="${opt.frame_url}">`
+<img alt="" id="frame-placeholder" src="data:image/png;base64,${opt.frame.placeholder}">
+<img alt="device frame" id="frame" src="${opt.frame.url}">`
 
+        this.tv_frame_placeholder = sr.querySelector('#frame-placeholder')
         this.tv_frame = sr.querySelector('#frame')
         this.tv_frame.onload = this.device_frame_load.bind(this)
         this.boot = 0
@@ -89,6 +110,14 @@ customElements.define('tv-program', class extends HTMLElement {
         }
 
         this.tv_snow = mkvid(this.tv_frame, "snow", "snow.mp4")
+        // once "snow" is ready, hide a placeholder & show a real device frame
+        this.tv_snow.addEventListener('canplay', () => {
+            setTimeout( () => {
+                // using a timer prevents "flashing" in android firefox
+                this.tv_frame_placeholder.style.display = 'none'
+            }, 100)
+            this.tv_frame.style.visibility = 'visible'
+        }, {once: true})
         this.tv_snow.autoplay = "true"
 
         this.tv_program = mkvid(this.tv_snow, "program", this.src)
